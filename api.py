c8013e8724436bb630145676c2a3bab1230e6af9
Bettina Kron <bettinakron@hotmail.com>
2022-12-16 11:55:35 +0100

    Menu, and some of the todo's

---
+++ api.py
@@ -0,0 +1,8 @@
+import requests
+
+
+def get_api(c):
+    """
+    Fetches data from API about nobel prizes, years etc
+    :return: list with info on nobel prizes
+    """
--- nobel_prize.py
+++ nobel_prize.py
@@ -1,13 +1,12 @@
 import requests
+import pprint
+from api import *
 
 # Tips: använd sidan nedan för att se vilken data vi får tillbaks och hur apiet fungerar
 # vi använder oss enbart av /nobelPrizes
 # Dokumentation, hjälp samt verktyg för att testa apiet fins här: https://app.swaggerhub.com/apis/NobelMedia/NobelMasterData/2.1
 
-HELP_STRING = """
-Ange ett år och fält
-Exempelvis 1965 fysik
-"""
+
 
 cat = {"fysik": "phy",
        "kemi": "che",
@@ -19,47 +18,100 @@ cat = {"fysik": "phy",
 
 
 # TODO 10p programmet skall ge en hjälpsam utskrift istället för en krasch om användaren skriver in fel input
-# TODO 15p om användaren inte anger ett område som exempelvis fysik eller kemi så skall den parametern inte skickas med till apiet och vi får då alla priser det året
-
-
-
-
 
 def main():
+    """
+    Menu with alternatives for Nobel prize information
+    :return:
+    """
+    HELP_STRING = """
+    Ange ett år och fält
+    Exempelvis 1965 fysik.
+    Följande ämnen finns att välja på:
+    fysik, kemi, litteratur, ekonomi, fred och medicin
+    
+    q) Avsluta programmet
+    h) Visa hjälptext
+    """
+    print(HELP_STRING)
 
     while True:
-        print(HELP_STRING)
-        # TODO 5p Skriv bara ut hjälptexten en gång när programmet startar inte efter varje gång användaren matat in en fråga
-        #      Förbättra hjälputskriften så att användaren vet vilka fält, exempelvis kemi som finns att välja på
+        choice = input("> ").lower()
+        if choice == "q":
+            choice_quit()
+            break
+        elif choice == "h":
+            show_help_text()
+        else:
+            list_input = choice.split()
+            if len(list_input) == 1:   # bara året
+                year = list_input[0]
+                category = ""
+            elif len(list_input) == 2:
+                year, subject = choice.split()
+                category = cat[subject]
 
-        # TODO 5p Gör så att det finns ett sätt att avsluta programmet, om användaren skriver Q så skall programmet stängas av
-        #      Beskriv i hjälptexten hur man avslutar programmet
-        # TODO 5p Gör så att hjälptexten skrivs ut om användaren skriver h eller H
-        aaa = input(">")
-        a, b = aaa.split()
-        c = cat[b]
+            search_param = {"nobelPrizeYear": int(year), "nobelPrizeCategory": category}
+
+            result = requests.get("http://api.nobelprize.org/2.1/nobelPrizes", params=search_param).json()  # result = dict
+            # pprint.pprint(result)
+            nobel_prizes(result)
 
 
-        c = {"nobelPrizeYear": int(a),"nobelPrizeCategory":c}
 
-        res = requests.get("http://api.nobelprize.org/2.1/nobelPrizes", params=c).json()
-        # TODO 5p  Lägg till någon typ av avskiljare mellan pristagare, exempelvis --------------------------
-
-        # TODO 20p Skriv ut hur mycket pengar varje pristagare fick, tänk på att en del priser delas mellan flera mottagare, skriv ut både i dåtidens pengar och dagens värde
+        # TODO 20p Skriv ut hur mycket pengar varje pristagare fick, tänk på att en del priser delas mellan flera mottagare,
+        #   skriv ut både i dåtidens pengar och dagens värde
         #   Skriv ut med tre decimalers precision. exempel 534515.123
         #   Skapa en funktion som hanterar uträkningen av prispengar och skapa minst ett enhetestest för den funktionen
         #   Tips, titta på variabeln andel
         # Feynman fick exempelvis 1/3 av priset i fysik 1965, vilket borde gett ungefär 282000/3 kronor i dåtidens penningvärde
 
-        for p in res["nobelPrizes"]:
-            peng = p["prizeAmount"]
-            idagpeng = p["prizeAmountAdjusted"]
-            print(f"{p['categoryFullName']['se']} prissumma {peng} SEK")
 
-            for m in p["laureates"]:
-                print(m['knownName']['en'])
-                print(m['motivation']['en'])
-                andel = m['portion']
+def nobel_prizes(nobel_info):
+    """
+    Extracted information on Nobel prizes
+    :param nobel_info: info from API
+    """
+    for prize in nobel_info['nobelPrizes']:
+        prize_amount = prize['prizeAmount']
+        adj_prize_amount = round((prize['prizeAmountAdjusted']), 3)
+        print(f"{prize['categoryFullName']['se']} prissumma {prize_amount} SEK (nuvarande penningvärde (SEK): "
+              f"{adj_prize_amount}) tilldelades:\n")
+        amt_of_winners = len(prize['laureates'])
+        winner_amount = round((prize_amount / amt_of_winners), 3)
+
+        for winner in prize['laureates']:
+            print(winner['knownName']['en'])
+            print(winner['motivation']['en'])
+            print(f"Amount of prize money: {winner_amount} ")
+            print(f"Prize money in today's money value: {adj_prize_amount}")
+            print("---"*30)
+            winner_share = winner['portion']
+
+
+def calculate_prize_amount():
+    pass
+
+
+def show_help_text():
+    """
+    Info on how to use the menu
+    """
+    print("""
+    Ange ett år och fält
+    Exempelvis 1965 fysik.
+    Följande ämnen finns att välja på:
+    fysik, kemi, litteratur, ekonomi, fred och medicin
+    
+    q) Avsluta programmet
+    """)
+
+def choice_quit():
+    """
+    Quits application
+    """
+    print("Hej då!")
+
 
 
 if __name__ == '__main__':
